e1aa318eb1cf165c5a104b352fd08fe340dbe25c
ovvladimir <ov.vladimir@mail.ru>
2020-04-26 14:06:47 +1000

    -

--- texture.py
+++ texture.py
@@ -1,34 +1,25 @@
 # https://imagemagick.org/index.php
-import sys
 import os
 from PIL import Image
 
-
-def append_images(images, aligment='bottom'):
-
-    widths, heights = zip(*(i.size for i in images))
-    print(widths, heights)
-
-    new_width = sum(widths)
-    new_height = max(heights)
-
-    new_im = Image.new('RGBA', (new_width, new_height))
-
-    x = 0
-    for j, im in enumerate(images):
-        if aligment == 'center':
-            y = (new_height - heights[j]) // 2
-        elif aligment == 'bottom':
-            y = new_height - heights[j]
-        new_im.paste(im, (x, y))
-        x += widths[j]
-
-    return new_im
-
-
+aligment = 'bottom'  # or 'center'
 path = 'in'
 images = [Image.open(path + os.sep + img) for img in os.listdir(path)]
-image = append_images(images)
+
+widths, heights = zip(*(i.size for i in images))
+print(widths, heights)
+new_width = sum(widths)
+new_height = max(heights)
+
+image = Image.new('RGBA', (new_width, new_height))
+x = 0
+for j, im in enumerate(images):
+    if aligment == 'center':
+        y = (new_height - heights[j]) // 2
+    elif aligment == 'bottom':
+        y = new_height - heights[j]
+    image.paste(im, (x, y))
+    x += widths[j]
+
 image.save('out/img.png')
 image.show()
-sys.exit(0)
