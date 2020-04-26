a4ac9ba4b74a46636bb18a40ab7625cee00b5f95
ovvladimir <ov.vladimir@mail.ru>
2020-04-26 16:12:31 +1000

    -

--- texture.py
+++ texture.py
@@ -7,15 +7,16 @@ path = 'in'
 images = [Image.open(path + os.sep + img) for img in os.listdir(path)]
 
 widths, heights = zip(*(i.size for i in images))
-print(widths, heights)
+W = [*widths]
 new_width = sum(widths)
 new_height = max(heights)
+print(widths, heights, f'\nW = {W}, H = {new_height}')
 
 image = Image.new('RGBA', (new_width, new_height))
 x = 0
 for j, im in enumerate(images):
     if aligment == 'center':
-        y = (new_height - heights[j]) // 2
+        y = round((new_height - heights[j]) / 2.)
     elif aligment == 'bottom':
         y = new_height - heights[j]
     image.paste(im, (x, y))
